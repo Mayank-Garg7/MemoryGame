6def98d31fe231cae41eb8eb35bdb3c2ef7a0ddc
Mayank Garg <113042462+Mayank-Garg7@users.noreply.github.com>
2026-07-06 21:11:08 +0530

    feat: add navigation section to make easy to navigate

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@
 
 const App = () => {
   return (
-    <div className="bg-gray-400 min-h-screen">
+    <div className="bg-blu-400 min-h-screen">
       <h1 className="text-white">hello welcome dude</h1>
     </div>
   )
--- src/shared/Navbar/index.tsx
+++ src/shared/Navbar/index.tsx
@@ -1,9 +1,13 @@
-import React from 'react'
-
+// import {Fa}
 const Navbar = () => {
   return (
     <div className='flex flex-row'>
-      
+      <div className="flex items-center flex-row">
+        <img src="" alt="" />
+        <p>Mayank</p>
+      </div>
+
+
     </div>
   )
 }
