e96abe99b6abc89da50d0e83e8a36188e16c51fb
Mayank Garg <113042462+Mayank-Garg7@users.noreply.github.com>
2026-07-11 22:26:20 +0530

    chore: implement the search functionality

--- src/shared/Button/SearchBar.tsx
+++ src/shared/Button/SearchBar.tsx
@@ -2,12 +2,12 @@ import { SlMagnifier } from "react-icons/sl";
 
 const SearchBox = () => {
     return (
-        <div className="flex items-center h-8 gap-4 px-4 rounded bg-blue-100 border-2 border-red-500">
-
+        <div className="flex items-center h-8 pl-5 gap-4 rounded bg-blue-100 ">
+            <SlMagnifier />
             <input
                 type="search"
-                placeholder="Search"
-                className="w-20 bg-transparent outline-none flex-end border-blue-600 border-2"
+                placeholder="Search here..."
+                className="bg-transparent outline-none"
             />
         </div>
     );
--- src/shared/Navbar/index.tsx
+++ src/shared/Navbar/index.tsx
@@ -1,10 +1,17 @@
 import image from "../../assets/logo.png";
 import { MdMenuOpen } from "react-icons/md";
 import SearchBox from "../Button/SearchBar";
+import { MdOutlineLightMode } from "react-icons/md";
+import { FaRegBell } from "react-icons/fa";
+import { IoCartOutline } from "react-icons/io5";
+import { MdOutlineEmail } from "react-icons/md";
+import IconButton from "@mui/material/IconButton";
+import profile from "../../assets/profile.jpg"
+
 
 const Navbar = () => {
   return (
-    <header className="flex items-center justify-between w-full h-12 bg-white px-4">
+    <header className="flex items-center justify-between w-full h-16 bg-white px-2">
       <div className="flex items-center gap-28">
         <div className="flex items-center">
           <img src={image} alt="Logo" className="h-10" />
@@ -12,14 +19,26 @@ const Navbar = () => {
         </div>
 
         <div className="flex items-center gap-4">
-          <button className="flex items-center justify-center w-8 h-8 rounded-full bg-blue-100">
+          <IconButton className="w-8 h-8 bg-blue-100" sx={{ backgroundColor: "#DBEAFE", "&:hover": { backgroundColor: "#BFDBFE" }, }}>
             <MdMenuOpen />
-          </button>
+          </IconButton>
           <SearchBox />
         </div>
       </div>
 
-      <div>hello</div>
+      <div className="flex flex-row gap-4 items-center ">
+        <IconButton className="w-8 h-8 bg-blue-100" sx={{ backgroundColor: "#DBEAFE", "&:hover": { backgroundColor: "#BFDBFE" }, }}><MdOutlineLightMode /></IconButton>
+        <IconButton className="w-8 h-8 bg-blue-100" sx={{ backgroundColor: "#DBEAFE", "&:hover": { backgroundColor: "#BFDBFE" }, }}><FaRegBell /></IconButton>
+        <IconButton className="w-8 h-8 bg-blue-100" sx={{ backgroundColor: "#DBEAFE", "&:hover": { backgroundColor: "#BFDBFE" }, }}><IoCartOutline /></IconButton>
+        <IconButton className="w-8 h-8 bg-blue-100" sx={{ backgroundColor: "#DBEAFE", "&:hover": { backgroundColor: "#BFDBFE" }, }}><MdOutlineEmail /></IconButton>
+        <div className="flex flex-row gap-2">
+          <img src={profile} alt="profile-picture" className="h-12 w-12 rounded-full border-4 border-blue-200" />
+          <div className="flex flex-col">
+            <span className="text-xl">Mayank Garg</span>
+            <p className="text-sm text-gray-400">@mayank-garg7</p>
+          </div>
+        </div>
+      </div>
     </header>
   );
 };
